3bf532903af4fa10f28178726cf5b03c3f387115
Oleksandr Polieno <polyenoom@gmail.com>
2025-02-20 08:49:29 +0700

    fix uow

--- presentation/12_repository.py
+++ presentation/12_repository.py
@@ -16,15 +16,13 @@ Cons:
 
 Repositories work with aggregates.
 """
-from contextlib import contextmanager
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
 from typing import Optional
 
 import sqlalchemy as sa
-from sqlalchemy import Connection
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, sessionmaker
 
 
 class EventTable(DeclarativeBase):
@@ -59,55 +57,34 @@ class Event:
     text: str
 
 
-class Database:
-    def __init__(self, connection: Connection):
-        self._connection = connection
-        self._session_factory = sessionmaker(bind=self._connection)
-
-    @contextmanager
-    def session(self, **kwargs):
-        session = self._session_factory(**kwargs)
-
-        try:
-            yield session
-            session.commit()
-        except Exception:
-            session.rollback()
-            raise
-        finally:
-            session.close()
-
-
 class EventsRepository:
-    def __init__(self, db: Database):
-        self._db = db
+    def __init__(self, session: Session):
+        self._session = session
 
     def save(self, event: Event) -> int:
-        with self._db.session() as session:
-            if event.id is None:
-                record = EventTable(
-                    type=event.type.value,
-                    text=event.text,
-                )
-            else:
-                record = session.query(EventTable).get(event.id)
-                record.type = event.type.value
-                record.text = event.text
+        if event.id is None:
+            record = EventTable(
+                type=event.type.value,
+                text=event.text,
+            )
+        else:
+            record = self._session.query(EventTable).get(event.id)
+            record.type = event.type.value
+            record.text = event.text
 
-            session.add(record)
+        self._session.add(record)
 
-            if record.id is None:
-                session.flush()
-                session.refresh(record)
+        if record.id is None:
+            self._session.flush()
+            self._session.refresh(record)
 
-            return record.id
+        return record.id
 
     def get(self, event_id: int) -> Event:
-        with self._db.session() as session:
-            record = session.query(EventTable).get(event_id)
+        record = self._session.query(EventTable).get(event_id)
 
-            return Event(
-                id=record.id,
-                type=EventType(record.type),
-                text=record.text,
-            )
+        return Event(
+            id=record.id,
+            type=EventType(record.type),
+            text=record.text,
+        )
--- presentation/15_unit_of_work.py
+++ presentation/15_unit_of_work.py
@@ -2,23 +2,24 @@
 """
 
 class UniOfWork:
-    def __init__(self, connection):
-        self._connection = connection
-        self.users_repo = UsersRepository(connection)
-        self.events_repo = EventsRepository(connection)
+    users_repo: UsersRepo
+    events_repo: EventsRepo
+
+    def __init__(self, db):
+        self._db = db
+        self._session = None
 
     def __enter__(self):
-        self._connection.begin()
+        self._session = self._db.session().__enter__()
+        self.users_repo = UsersRepo(self._session)
+        self.events_repo = EventsRepo(self._session)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type is None:
-            self._connection.commit()
-        else:
-            self._connection.rollback()
+        self._session.__exit__(exc_type, exc_val, exc_tb)
 
 
-uow = UniOfWork(connection)
+uow = UniOfWork(db)
 with uow:
     uow.users_repo.save(user)
     uow.events_repo.save(event)
